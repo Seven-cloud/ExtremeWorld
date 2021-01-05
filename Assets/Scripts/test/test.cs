455083d63d17f65aec6c541965797501c0ab1dca
Seven-cloud <1379022680@qq.com>
2021-01-05 19:16:27 +0800

    1
    1

---
+++ Assets/Scripts/test/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Class1 c1 = new Class1();
+        int p =  c1.fn(1, 2);
+        Debug.Log("方法:"+p);
+        Debug.Log("属性:"+ c1.a);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
